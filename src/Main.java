a757e2e0c21aa6148e4c2162d8d33ab44bf9efd5
xAntonioJr98 <actorresjunior@gmail.com>
2025-05-20 21:57:28 -0300

    classe tabuleiro criada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import Tabuleiro.Posicao;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Posicao posicao = new Posicao(1, 2);
+
+        System.out.println("Linha: " + posicao.getLinha() + " Coluna: " + posicao.getColuna());
     }
 }
